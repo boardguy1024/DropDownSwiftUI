49b46639c36bbe648cdca4db377d98a49c44041d
boardguy1024 <boardguy1024@gmail.com>
2023-11-14 11:57:41 +0900

    Initial Commit

---
+++ DropDownSwiftUI/DropDownSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropDownSwiftUIApp.swift
+//  DropDownSwiftUI
+//
+//  Created by park kyung seok on 2023/11/14.
+//
+
+import SwiftUI
+
+@main
+struct DropDownSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
